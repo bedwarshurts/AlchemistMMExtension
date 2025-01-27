c2e8cef5eb7a0156a42986e54f6754f42e1dde57
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-01-27 15:23:05 +0200

    Missing annotation

--- src/main/java/me/bedwarshurts/mmextension/conditions/IsInFactionCondition.java
+++ src/main/java/me/bedwarshurts/mmextension/conditions/IsInFactionCondition.java
@@ -5,8 +5,10 @@ import io.lumine.mythic.api.config.MythicLineConfig;
 import io.lumine.mythic.api.skills.conditions.IEntityCondition;
 import io.lumine.mythic.bukkit.MythicBukkit;
 import io.lumine.mythic.core.mobs.ActiveMob;
+import io.lumine.mythic.core.utils.annotations.MythicCondition;
 import org.bukkit.entity.Entity;
 
+@MythicCondition(author = "bedwarshurts", name = "isinfaction", aliases = {}, description = "Check if the entity is in a specified faction")
 public class IsInFactionCondition implements IEntityCondition {
     private final String faction;
 
