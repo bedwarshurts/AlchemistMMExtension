792ef17c6491ced28f38db3b5b6ca9b4171db006
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-03-08 20:26:53 +0200

    add placeholder support on @TPL

--- src/main/java/me/bedwarshurts/mmextension/targeters/LocationPredictingTargeter.java
+++ src/main/java/me/bedwarshurts/mmextension/targeters/LocationPredictingTargeter.java
@@ -4,6 +4,7 @@ import io.lumine.mythic.api.adapters.AbstractEntity;
 import io.lumine.mythic.api.adapters.AbstractLocation;
 import io.lumine.mythic.api.config.MythicLineConfig;
 import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.api.skills.placeholders.PlaceholderDouble;
 import io.lumine.mythic.api.skills.targeters.ILocationTargeter;
 import io.lumine.mythic.core.utils.annotations.MythicTargeter;
 import io.lumine.mythic.bukkit.utils.serialize.Position;
@@ -24,30 +25,29 @@ import java.util.Objects;
 @MythicTargeter(author = "bedwarshurts", name = "targetpredictedlocation", aliases = {"TPL"}, description = "Predicts the location of the target")
 public class LocationPredictingTargeter implements ILocationTargeter {
 
-    private final double predictionTimeSeconds;
+    private final PlaceholderDouble predictionTimeSeconds;
     private final double yOffset;
     private final boolean ignoreY;
     private final boolean ignoreIfStill;
     private final Map<UUID, Location> previousLocations = new HashMap<>();
 
     public LocationPredictingTargeter(MythicLineConfig mlc) {
-        this.predictionTimeSeconds = mlc.getDouble("time", 1.0);
+        this.predictionTimeSeconds = PlaceholderDouble.of(mlc.getString("time", String.valueOf(1.0)));
         this.yOffset = mlc.getDouble("y", 0.0);
         this.ignoreY = mlc.getBoolean(new String[]{"ignoreY","iy"}, false);
         this.ignoreIfStill = mlc.getBoolean(new String[]{"ignoreStill","is"}, false);
     }
 
     @Override
-    public Collection<AbstractLocation> getLocations(SkillMetadata skillMetadata) {
+    public Collection<AbstractLocation> getLocations(SkillMetadata data) {
         Set<AbstractLocation> locations = new HashSet<>();
 
-        for (AbstractEntity targetEntity : skillMetadata.getEntityTargets()) {
+        for (AbstractEntity targetEntity : data.getEntityTargets()) {
             Entity bukkitEntity = targetEntity.getBukkitEntity();
             UUID entityId = bukkitEntity.getUniqueId();
             Location currentLocation = bukkitEntity.getLocation();
             Location previousLocation = previousLocations.getOrDefault(entityId, currentLocation);
 
-            // Calculate the direction based on the difference between current and previous locations
             Vector direction = currentLocation.toVector().subtract(previousLocation.toVector()).normalize();
 
             // Always update the previous location
@@ -70,7 +70,7 @@ public class LocationPredictingTargeter implements ILocationTargeter {
                     targetLocation = currentLocation.clone();
                 }
             } else {
-                Vector predictedMovement = direction.multiply(speedBps * predictionTimeSeconds);
+                Vector predictedMovement = direction.multiply(speedBps * predictionTimeSeconds.get(data));
                 targetLocation = currentLocation.clone().add(predictedMovement);
                 targetLocation.setY(targetLocation.getY() + yOffset);
             }
@@ -78,7 +78,6 @@ public class LocationPredictingTargeter implements ILocationTargeter {
             Position position = Position.of(targetLocation);
             locations.add(new AbstractLocation(position));
 
-            // Update the previous location
             previousLocations.put(entityId, currentLocation);
         }
 
