c8d09c5a6a9e6fea3433b9a8d2c573a295198ff5
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-03-14 02:22:27 +0200

    improve debug

--- src/main/java/me/bedwarshurts/mmextension/mechanics/canceldeath/CancelPlayerDeathMechanic.java
+++ src/main/java/me/bedwarshurts/mmextension/mechanics/canceldeath/CancelPlayerDeathMechanic.java
@@ -6,16 +6,16 @@ import io.lumine.mythic.api.skills.*;
 import io.lumine.mythic.core.utils.annotations.MythicMechanic;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.WeakHashMap;
 
 @MythicMechanic(author = "bedwarshurts", name = "cancelplayerdeath", aliases = {}, description = "Cancels the player's next death if activated")
 public class CancelPlayerDeathMechanic implements ITargetedEntitySkill {
     private final double healthPercentage;
     private final String skillName;
 
-    public static final Map<UUID,PlayerDeathData> playerDeathDatas = new WeakHashMap<>();
+    public static final Map<UUID, PlayerDeathData> playerDeathDatas = new HashMap<>();
 
     public CancelPlayerDeathMechanic(MythicLineConfig mlc) {
         this.healthPercentage = mlc.getDouble("healthPercentage", 100.0);
@@ -24,14 +24,14 @@ public class CancelPlayerDeathMechanic implements ITargetedEntitySkill {
 
     @Override
     public SkillResult castAtEntity(SkillMetadata data, AbstractEntity target) {
-        if (target.getBukkitEntity() instanceof Player player) {
-            if (playerDeathDatas.containsKey(player.getUniqueId())) {
-                playerDeathDatas.remove(player.getUniqueId());
-            } else {
-                playerDeathDatas.put(player.getUniqueId(), new PlayerDeathData(player.getUniqueId(), healthPercentage, skillName, data));
-            }
+        if (!(target.getBukkitEntity() instanceof Player player)) return SkillResult.INVALID_TARGET;
+
+        if (playerDeathDatas.containsKey(player.getUniqueId())) {
+            playerDeathDatas.remove(player.getUniqueId());
+
             return SkillResult.SUCCESS;
         }
-        return SkillResult.CONDITION_FAILED;
+        playerDeathDatas.put(player.getUniqueId(), new PlayerDeathData(player.getUniqueId(), healthPercentage, skillName, data));
+        return SkillResult.SUCCESS;
     }
 }
