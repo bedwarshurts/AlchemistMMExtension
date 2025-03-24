b0ba63ea17337cf95ff8cc865e3f197a885646a8
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-03-24 18:18:27 +0200

    add MythicSkill class to describe Mythic skills

---
+++ src/main/java/me/bedwarshurts/mmextension/utils/MythicSkill.java
@@ -0,0 +1,46 @@
+package me.bedwarshurts.mmextension.utils;
+
+import io.lumine.mythic.api.skills.Skill;
+import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.bukkit.BukkitAdapter;
+import io.lumine.mythic.bukkit.MythicBukkit;
+import io.lumine.mythic.core.skills.SkillExecutor;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+
+import java.util.Optional;
+
+public class MythicSkill {
+    private final Skill skill;
+
+    public MythicSkill(String skillName) {
+        this(skillName, MythicBukkit.inst().getSkillManager());
+    }
+
+    public MythicSkill(String skillName, SkillExecutor manager) {
+        if (skillName.isEmpty()) {
+            this.skill = null;
+            return;
+        }
+        Optional<Skill> skillOptional = manager.getSkill(skillName);
+        if (skillOptional.isEmpty()) {
+            throw new NullPointerException("Skill not found");
+        }
+
+        this.skill = skillOptional.get();
+    }
+
+    public void cast(SkillMetadata data) {
+        skill.execute(data);
+    }
+
+    public void castAtPoint(SkillMetadata data, Location location) {
+        data.setLocationTarget(BukkitAdapter.adapt(location));
+        skill.execute(data);
+    }
+
+    public void castAtEntity(SkillMetadata data, Entity entity) {
+        data.setEntityTarget(BukkitAdapter.adapt(entity));
+        skill.execute(data);
+    }
+}
