86b925b265b30643ad32375e40eb9115bb463df6
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-04-07 16:42:16 +0300

    removed startup warnings, now if a mechanic requires a dependency an exception will be thrown

--- src/main/java/me/bedwarshurts/mmextension/AlchemistMMExtension.java
+++ src/main/java/me/bedwarshurts/mmextension/AlchemistMMExtension.java
@@ -6,6 +6,7 @@ import me.bedwarshurts.mmextension.comp.PlaceholderAPIHook;
 import me.bedwarshurts.mmextension.listeners.EntityDamageListener;
 import me.bedwarshurts.mmextension.listeners.PlayerChangeSlotListener;
 import me.bedwarshurts.mmextension.listeners.PlayerSkillCastListener;
+import me.bedwarshurts.mmextension.utils.exceptions.DependencyNotFoundException;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mariuszgromada.math.mxparser.License;
@@ -13,6 +14,12 @@ import org.mariuszgromada.math.mxparser.License;
 import java.util.Objects;
 
 public class AlchemistMMExtension extends JavaPlugin {
+    private boolean isMMOItems = true;
+    private boolean isMythicLib = true;
+    private boolean isMMOCore = true;
+    private boolean isPlaceholderAPI = true;
+    private boolean isProtocolLib = true;
+
     private static AlchemistMMExtension plugin;
 
     static {
@@ -20,46 +27,62 @@ public class AlchemistMMExtension extends JavaPlugin {
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public void onEnable() {
         plugin = this;
 
         if (Bukkit.getPluginManager().getPlugin("MythicMobs") == null) {
-            getLogger().severe("MythicMobs is not installed! Disabling AlchemistMMExtension...");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
+            throw new DependencyNotFoundException("xd? you seriously installed an extension for mythicmobs without mythicmobs");
         }
 
         if (Bukkit.getPluginManager().getPlugin("MMOItems") == null) {
-            getLogger().warning("MMOItems is not installed! Some features may not work.");
+            this.isMMOItems = false;
         }
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
-            getLogger().warning("PlaceholderAPI is not installed! Placeholder features will not work.");
+            this.isPlaceholderAPI = false;
         }
 
         if (Bukkit.getPluginManager().getPlugin("MythicLib") == null) {
-            getLogger().warning("MythicLib is not installed! Some features may not work.");
+            this.isMythicLib = false;
         }
 
         if (Bukkit.getPluginManager().getPlugin("MMOCore") == null) {
-            getLogger().warning("MMOCore is not installed! Some features may not work.");
+            this.isMMOCore = false;
+        }
+
+        if (Bukkit.getPluginManager().getPlugin("ProtocolLib") == null) {
+            this.isProtocolLib = false;
         }
 
         Bukkit.getPluginManager().registerEvents(new MythicMobsHook(), this);
         Bukkit.getPluginManager().registerEvents(new EntityDamageListener(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerSkillCastListener(), this);
+        if (isMMOCore && isMythicLib) Bukkit.getPluginManager().registerEvents(new PlayerSkillCastListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerChangeSlotListener(), this);
 
         Objects.requireNonNull(this.getCommand("playerspawnmythicmob")).setExecutor(new PlayerSpawnMythicMobCommand());
 
-        new PlaceholderAPIHook().register();
+        if (isPlaceholderAPI) new PlaceholderAPIHook().register();
 
         getLogger().info("AlchemistMMExtension has been enabled! Made by bedwarshurts");
+        getLogger().info("Version: " + plugin.getDescription());
     }
 
     @Override
     public void onDisable() {
         getLogger().info("AlchemistMMExtension has been disabled!");
+    }
+
+    public boolean isMMOItems() {
+        return isMMOItems;
+    }
+
+    public boolean isProtocolLib() {
+        return isProtocolLib;
+    }
+
+    public boolean isMMOCore() {
+        return isProtocolLib;
     }
 
     public static AlchemistMMExtension inst() {
