9b2d31a31f056c8720bce225e320f6fdc882012a
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-04-10 03:10:16 +0300

    recode change slot signal

--- src/main/java/me/bedwarshurts/mmextension/listeners/SkillTriggerListeners.java
+++ src/main/java/me/bedwarshurts/mmextension/listeners/SkillTriggerListeners.java
@@ -8,16 +8,17 @@ import io.lumine.mythic.core.skills.EventExecutor;
 import io.lumine.mythic.core.skills.TriggeredSkill;
 import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
 import me.bedwarshurts.mmextension.skills.triggers.MoreSkillTriggers;
+import me.bedwarshurts.mmextension.skills.triggers.meta.PlayerChangeSlotMeta;
 import me.bedwarshurts.mmextension.skills.triggers.meta.PreAttackEntityMeta;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 
 public class SkillTriggerListeners implements Listener {
     private final EventExecutor eventExecutor = MythicBukkit.inst().getSkillManager().getEventBus();
 
-    // ~onPreAttack
     @EventHandler
     public void onPreAttackEntityTrigger(PrePlayerAttackEntityEvent event) {
         Entity target = event.getAttacked();
@@ -32,6 +33,23 @@ public class SkillTriggerListeners implements Listener {
         TriggeredSkill ts = eventExecutor.processTriggerMechanics(data, triggerMeta);
         if (ts.getCancelled()) {
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerChangeSlotTrigger(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        for (Entity entity : player.getNearbyEntities(30, 30, 30)) {
+            if (!MythicBukkit.inst().getAPIHelper().isMythicMob(entity)) continue;
+            ActiveMob am = MythicBukkit.inst().getAPIHelper().getMythicMobInstance(entity);
+
+            PlayerChangeSlotMeta triggerMeta = new PlayerChangeSlotMeta(event);
+            SkillMetadata data = eventExecutor.buildSkillMetadata(MoreSkillTriggers.PLAYER_CHANGE_SLOT, triggerMeta, am,
+                    BukkitAdapter.adapt(player), BukkitAdapter.adapt(player.getLocation()), true);
+            TriggeredSkill ts = eventExecutor.processTriggerMechanics(data, triggerMeta);
+            if (ts.getCancelled()) {
+                event.setCancelled(true);
+            }
         }
     }
 }
---
+++ src/main/java/me/bedwarshurts/mmextension/skills/triggers/meta/PlayerChangeSlotMeta.java
@@ -0,0 +1,21 @@
+package me.bedwarshurts.mmextension.skills.triggers.meta;
+
+import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.core.skills.triggers.SkillTriggerMetadata;
+import io.lumine.mythic.core.skills.variables.types.StringVariable;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+
+public class PlayerChangeSlotMeta extends SkillTriggerMetadata {
+    private final PlayerItemHeldEvent event;
+
+    public PlayerChangeSlotMeta(PlayerItemHeldEvent event) {
+        this.event = event;
+    }
+
+    @Override
+    public void applyToSkillMetadata(SkillMetadata data) {
+        data.getVariables().put("trigger", new StringVariable(event.getPlayer().getName()));
+        data.getVariables().put("previousSlot", new StringVariable(String.valueOf(event.getPreviousSlot())));
+        data.getVariables().put("currentSlot", new StringVariable(String.valueOf(event.getNewSlot())));
+    }
+}
