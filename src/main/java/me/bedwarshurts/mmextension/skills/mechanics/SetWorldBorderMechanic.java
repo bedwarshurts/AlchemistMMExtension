b78cfd18ad9468a34f4ae5d8b7a2c389c990bd93
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-04-11 18:49:27 +0300

    fix coq and optimise the logic

--- src/main/java/me/bedwarshurts/mmextension/skills/mechanics/SetWorldBorderMechanic.java
+++ src/main/java/me/bedwarshurts/mmextension/skills/mechanics/SetWorldBorderMechanic.java
@@ -7,7 +7,9 @@ import io.lumine.mythic.api.skills.SkillMetadata;
 import io.lumine.mythic.api.skills.SkillResult;
 import io.lumine.mythic.api.skills.placeholders.PlaceholderDouble;
 import io.lumine.mythic.api.skills.placeholders.PlaceholderInt;
+import io.lumine.mythic.bukkit.BukkitAdapter;
 import io.lumine.mythic.core.utils.annotations.MythicMechanic;
+import me.bedwarshurts.mmextension.AlchemistMMExtension;
 import me.bedwarshurts.mmextension.utils.events.Events;
 import me.bedwarshurts.mmextension.utils.terminable.TerminableConsumer;
 import me.bedwarshurts.mmextension.utils.terminable.TerminableRegistry;
@@ -20,8 +22,9 @@ import org.bukkit.event.player.PlayerJoinEvent;
 @MythicMechanic(author = "bedwarshurts", name = "setworldborder", aliases = {"swb"}, description = "Sets the world border for a player")
 public class SetWorldBorderMechanic implements ITargetedEntitySkill, TerminableConsumer {
     private final TerminableRegistry consumer = new TerminableRegistry();
-    private final PlaceholderDouble radius;;
+    private final PlaceholderDouble radius;
     private final boolean cancelOnQuit;
+    private final boolean remove;
     private final PlaceholderInt timeTicks;
     private final PlaceholderDouble damageAmount;
     private final PlaceholderDouble damageBuffer;
@@ -30,6 +33,7 @@ public class SetWorldBorderMechanic implements ITargetedEntitySkill, TerminableC
 
     public SetWorldBorderMechanic(MythicLineConfig mlc) {
         this.radius = mlc.getPlaceholderDouble(new String[]{"radius", "r"}, "0");
+        this.remove = mlc.getBoolean(new String[]{"remove"}, false);
         this.damageAmount = mlc.getPlaceholderDouble(new String[]{"damageAmount", "da"}, 0.2);
         this.damageBuffer = mlc.getPlaceholderDouble(new String[]{"damageBuffer", "db"}, 2);
         this.warningTicks = mlc.getPlaceholderInteger(new String[]{"warningTicks", "wt"}, 15);
@@ -43,33 +47,31 @@ public class SetWorldBorderMechanic implements ITargetedEntitySkill, TerminableC
         if (!target.isPlayer()) return SkillResult.INVALID_TARGET;
         Player player = (Player) target.getBukkitEntity();
 
-        if (player.getWorldBorder() != null && timeTicks.get(data, data.getCaster().getEntity()) == 0) {
-            player.setWorldBorder(null);
-            consumer.close();
-
+        if (player.getWorldBorder() != null) {
+            if (remove) {
+                player.setWorldBorder(null);
+                consumer.close();
+            } else {
+                player.getWorldBorder().setSize(radius.get(data), timeTicks.get(data, data.getCaster().getEntity()) / 20);
+            }
             return SkillResult.SUCCESS;
         }
 
         WorldBorder border = Bukkit.createWorldBorder();
-        border.setCenter(player.getLocation());
         border.setCenter(BukkitAdapter.adapt(data.getCaster().getLocation()));
         border.setWarningTime(warningTicks.get(data, data.getCaster().getEntity()) / 20);
         border.setDamageBuffer(damageBuffer.get(data, data.getCaster().getEntity()));
         border.setWarningDistance(warningDistance.get(data, data.getCaster().getEntity()));
         border.setDamageAmount(damageAmount.get(data, data.getCaster().getEntity()));
-
-        if (timeTicks.get(data, data.getCaster().getEntity()) == 0) {
-            border.setSize(radius.get(data));
-        } else {
-            border.setSize(radius.get(data), timeTicks.get(data, data.getCaster().getEntity()) / 20);
-        }
+        border.setSize(radius.get(data));
 
         player.setWorldBorder(border);
 
         if (!cancelOnQuit) {
             Events.subscribe(PlayerJoinEvent.class, EventPriority.NORMAL)
-                    .filter(e -> e.getPlayer().getUniqueId().equals(player.getUniqueId()))
-                    .handler(e -> player.setWorldBorder(border))
+                    .filter(e -> e.getPlayer().getUniqueId().toString().equals(player.getUniqueId().toString()))
+                    .handler(e -> Bukkit.getScheduler().runTaskLater(AlchemistMMExtension.inst(),
+                            () -> e.getPlayer().setWorldBorder(border), 40L))
                     .bindWith(this);
         }
         return SkillResult.SUCCESS;
