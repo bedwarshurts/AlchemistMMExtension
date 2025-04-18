3a0dc9dbf84dca5f413151a0cda7d384b2c316f1
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-04-18 22:52:43 +0300

    rework HideChatMechanic

--- src/main/java/me/bedwarshurts/mmextension/skills/mechanics/HideChatMechanic.java
+++ src/main/java/me/bedwarshurts/mmextension/skills/mechanics/HideChatMechanic.java
@@ -7,22 +7,25 @@ import io.lumine.mythic.api.skills.SkillMetadata;
 import io.lumine.mythic.api.skills.SkillResult;
 import io.lumine.mythic.api.skills.placeholders.PlaceholderInt;
 import io.lumine.mythic.core.utils.annotations.MythicMechanic;
+import io.papermc.paper.event.player.AsyncChatEvent;
 import me.bedwarshurts.mmextension.AlchemistMMExtension;
-import me.bedwarshurts.mmextension.listeners.ChatListener;
+import me.bedwarshurts.mmextension.utils.events.Events;
+import me.bedwarshurts.mmextension.utils.terminable.TerminableConsumer;
+import me.bedwarshurts.mmextension.utils.terminable.TerminableRegistry;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventPriority;
 
-import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 @MythicMechanic(author = "bedwarshurts", name = "hidechat", description = "Hide the chat for a player")
-public class HideChatMechanic implements ITargetedEntitySkill {
+public class HideChatMechanic implements ITargetedEntitySkill, TerminableConsumer {
     private final PlaceholderInt duration;
+    private final TerminableRegistry consumer = new TerminableRegistry();
 
-    public static HashSet<Player> playerChatList = new HashSet<>();
+    public static ConcurrentHashMap<Player, TerminableRegistry> playerChatList = new ConcurrentHashMap<>();
 
     public HideChatMechanic(MythicLineConfig mlc) {
-        Bukkit.getPluginManager().registerEvents(new ChatListener(), AlchemistMMExtension.inst());
-
         this.duration = PlaceholderInt.of(String.valueOf(mlc.getInteger(new String[]{"duration", "d"}, 0)));
     }
 
@@ -30,16 +33,28 @@ public class HideChatMechanic implements ITargetedEntitySkill {
     public SkillResult castAtEntity(SkillMetadata data, AbstractEntity target) {
         if (!target.isPlayer()) return SkillResult.INVALID_TARGET;
 
-        if (playerChatList.contains((Player) target.getBukkitEntity())) {
+        if (playerChatList.containsKey((Player) target.getBukkitEntity())) {
+            playerChatList.get((Player) target.getBukkitEntity()).close();
             playerChatList.remove((Player) target.getBukkitEntity());
-        } else {
-            playerChatList.add((Player) target.getBukkitEntity());
+            return SkillResult.SUCCESS;
         }
 
-        Bukkit.getScheduler().runTaskLaterAsynchronously(AlchemistMMExtension.inst(), () -> {
-            playerChatList.remove((Player) target.getBukkitEntity());
-        }, duration.get(data));
+        Events.subscribe(AsyncChatEvent.class, EventPriority.NORMAL)
+                .filter(event -> event.getPlayer().getUniqueId().equals(target.getUniqueId()))
+                .handler(event -> {
+                    if (event.getPlayer().hasPermission("alchemist.bypasshiddenchat")) return;
+
+                    event.viewers().remove(target.getBukkitEntity());
+                })
+                .bindWith(this);
+
+        Bukkit.getScheduler().runTaskLaterAsynchronously(AlchemistMMExtension.inst(), consumer::close, duration.get(data));
 
         return SkillResult.SUCCESS;
+    }
+
+    @Override
+    public TerminableConsumer with(AutoCloseable terminable) {
+        return consumer.with(terminable);
     }
 }
