607292f126cdd4661e9ceffebc52b1cafc5e3273
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-04-28 21:47:30 +0300

    add debug mechanic

---
+++ src/main/java/me/bedwarshurts/mmextension/skills/mechanics/TestMechanic.java
@@ -0,0 +1,40 @@
+package me.bedwarshurts.mmextension.skills.mechanics;
+
+import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.api.skills.INoTargetSkill;
+import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.api.skills.SkillResult;
+import io.lumine.mythic.core.utils.annotations.MythicMechanic;
+import me.bedwarshurts.mmextension.utils.PlaceholderUtils;
+import me.clip.placeholderapi.PlaceholderAPI;
+
+import java.util.Map;
+import java.util.Set;
+
+@MythicMechanic(author = "bedwarshurts", name = "test", aliases = {}, description = "Test mechanic for debugging purposes")
+public class TestMechanic implements INoTargetSkill {
+    private String line;
+    private final String key;
+    private final Set<Map.Entry<String, String>> map;
+
+    public TestMechanic(MythicLineConfig mlc) {
+        this.line = mlc.getLine();
+        this.key = mlc.getKey();
+        this.map = mlc.entrySet();
+    }
+
+    @Override
+    public SkillResult cast(SkillMetadata data) {
+        line = PlaceholderUtils.parseStringPlaceholders(line, data);
+        line = PlaceholderAPI.setPlaceholders(null, line);
+        System.out.println(line);
+        System.out.println("-----------------------------");
+        System.out.println(key);
+        System.out.println("------------------------------");
+        for (Map.Entry<String, String> entry : map) {
+            System.out.println(entry.getValue());
+        }
+
+        return SkillResult.SUCCESS;
+    }
+}
