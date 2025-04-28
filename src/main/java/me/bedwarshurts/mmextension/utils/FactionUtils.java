8a06e4a4fd125ab93936f52a6c120b7fd0fef1e5
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-04-28 21:46:56 +0300

    improve faction check performance

--- src/main/java/me/bedwarshurts/mmextension/utils/FactionUtils.java
+++ src/main/java/me/bedwarshurts/mmextension/utils/FactionUtils.java
@@ -8,16 +8,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import java.util.WeakHashMap;
 
 public final class FactionUtils {
+
+    private static final WeakHashMap<Entity, String> factionCache = new WeakHashMap<>();
 
     private FactionUtils() {
         throw new UnsupportedOperationException("You really shouldnt initialise this class");
     }
 
     private static String getFaction(Entity entity) {
-        ActiveMob mob = MythicBukkit.inst().getMobManager().getMythicMobInstance(entity);
-        return mob != null && mob.hasFaction() ? mob.getFaction() : null;
+        return factionCache.computeIfAbsent(entity, e -> {
+            ActiveMob mob = MythicBukkit.inst().getMobManager().getMythicMobInstance(e);
+            return mob != null && mob.hasFaction() ? mob.getFaction() : null;
+        });
     }
 
     public static void applyExtraDamage(EntityDamageEvent event) {
