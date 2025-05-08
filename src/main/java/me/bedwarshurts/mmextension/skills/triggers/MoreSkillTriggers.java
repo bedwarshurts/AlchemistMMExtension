435e123b05058af86a01ad1f1cd0719286aadcf2
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-05-08 16:20:46 +0300

    remove unnecessary aliases

--- src/main/java/me/bedwarshurts/mmextension/skills/triggers/MoreSkillTriggers.java
+++ src/main/java/me/bedwarshurts/mmextension/skills/triggers/MoreSkillTriggers.java
@@ -8,11 +8,11 @@ import me.bedwarshurts.mmextension.skills.triggers.meta.SkillCastMeta;
 public class MoreSkillTriggers {
 
     public static final SkillTrigger<?> PRE_ATTACK =
-            SkillTrigger.create("PRE_ATTACK", PreAttackEntityMeta.class, "PREATTACK", "PRE_ATTACK", "PREATTACKENTITY", "PRE_ATTACK_ENTITY");
+            SkillTrigger.create("PRE_ATTACK", PreAttackEntityMeta.class, "PREATTACK", "PREATTACKENTITY", "PRE_ATTACK_ENTITY");
     public static final SkillTrigger<?> SKILL_CAST =
-            SkillTrigger.create("SKILL_CAST", SkillCastMeta.class, "SKILLCAST", "SKILL_CAST", "MMOSKILLCAST", "MMO_SKILL_CAST");
+            SkillTrigger.create("SKILL_CAST", SkillCastMeta.class, "SKILLCAST", "MMOSKILLCAST", "MMO_SKILL_CAST");
     public static final SkillTrigger<?> PLAYER_CHANGE_SLOT =
-            SkillTrigger.create("PLAYER_CHANGE_SLOT", PlayerChangeSlotMeta.class, "PLAYERCHANGESLOT", "PLAYER_CHANGE_SLOT", "CHANGE_SLOT", "CHANGESLOT");
+            SkillTrigger.create("PLAYER_CHANGE_SLOT", PlayerChangeSlotMeta.class, "PLAYERCHANGESLOT", "CHANGE_SLOT", "CHANGESLOT");
 
     public static void registerTriggers() {
         PRE_ATTACK.register();
