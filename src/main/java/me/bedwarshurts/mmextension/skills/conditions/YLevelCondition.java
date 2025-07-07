c2bc03c776dd0f153ca9ce0240ce37e6e04dc01f
Shattered Visions <138602987+bedwarshurts@users.noreply.github.com>
2025-07-07 14:14:28 +0300

    add > < operator support to YLevelCondition

--- src/main/java/me/bedwarshurts/mmextension/skills/conditions/YLevelCondition.java
+++ src/main/java/me/bedwarshurts/mmextension/skills/conditions/YLevelCondition.java
@@ -4,19 +4,25 @@ import io.lumine.mythic.api.config.MythicLineConfig;
 import io.lumine.mythic.api.skills.SkillMetadata;
 import io.lumine.mythic.api.skills.conditions.ISkillMetaCondition;
 import io.lumine.mythic.core.utils.annotations.MythicCondition;
-import io.lumine.mythic.api.skills.placeholders.PlaceholderInt;
+import me.bedwarshurts.mmextension.utils.PlaceholderUtils;
 
 @MythicCondition(author = "bedwarshurts", name = "isylevel", aliases = {}, description = "Check if the origin is at a certain Y level")
 public class YLevelCondition implements ISkillMetaCondition {
 
-    private final PlaceholderInt yLevel;
+    private final String yLevel;
 
     public YLevelCondition(MythicLineConfig mlc) {
-        this.yLevel = PlaceholderInt.of(mlc.getString("y", "0"));
+        this.yLevel = mlc.getString("y", "0");
     }
 
     @Override
     public boolean check(SkillMetadata data) {
-        return data.getOrigin().getBlockY() == yLevel.get(data);
+        String yLevelParsed = PlaceholderUtils.parseDoublePlaceholders(yLevel, data);
+
+        return switch (yLevelParsed.charAt(0)) {
+            case '<' -> data.getOrigin().getBlockY() < Integer.parseInt(yLevelParsed.substring(1));
+            case '>' -> data.getOrigin().getBlockY() > Integer.parseInt(yLevelParsed.substring(1));
+            default -> data.getOrigin().getBlockY() == Integer.parseInt(yLevelParsed);
+        };
     }
 }
